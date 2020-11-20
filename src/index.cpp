7f9dbe107a96bd6cbc4dbad8aec9586abd152414
Justin Kambic <jek5104@gmail.com>
2020-11-20 00:29:39 -0500

    Add exception for path to nonexistant file.

--- src/index.cpp
+++ src/index.cpp
@@ -1,6 +1,5 @@
 #include "libraw_wrapper.h"
 
-
 Napi::Object Init(Napi::Env env, Napi::Object exports)
 {
   return LibRawWrapper::Init(env, exports);
--- src/libraw_wrapper.cpp
+++ src/libraw_wrapper.cpp
@@ -1,6 +1,7 @@
 #include <napi.h>
 #include "libraw_wrapper.h"
 #include "wraptypes.h"
+#include <fstream>
 
 Napi::Object LibRawWrapper::Init(Napi::Env& env, Napi::Object& exports) {
   Napi::HandleScope scope(env);
@@ -23,26 +24,37 @@ Napi::Object LibRawWrapper::Init(Napi::Env& env, Napi::Object& exports) {
 LibRawWrapper::LibRawWrapper(const Napi::CallbackInfo& info): Napi::ObjectWrap<LibRawWrapper>(info) {
   Napi::Env env = info.Env();
   Napi::HandleScope scope(env);
-  if (info.Length() < 1) {
+  if (info.Length() < 1)
+  {
     Napi::TypeError::New(env, "Expected path to image").ThrowAsJavaScriptException();
     return;
   }
-  if (!info[0].IsString() && (!info[0].IsBuffer() && info.Length() > 1)) {
+  if (!info[0].IsString() && (!info[0].IsBuffer() && info.Length() > 1))
+  {
     Napi::TypeError::New(env, "Expected path to image or buffer with size").ThrowAsJavaScriptException();
     return;
   }
 
-  if (info[0].IsString()) {
-    this->processor_ = new LibRaw();
+  if (info[0].IsString())
+  {
     Napi::String path = info[0].As<Napi::String>();
+    std::fstream f(path.Utf8Value().c_str());
+    if (!f.good())
+    {
+      Napi::TypeError::New(env, "File does not exist").ThrowAsJavaScriptException();
+      return;
+    }
+    this->processor_ = new LibRaw();
     this->processor_->open_file(path.Utf8Value().c_str());
   }
-  else if (info[0].IsBuffer() && info[1].IsNumber()) {
+  else if (info[0].IsBuffer() && info[1].IsNumber())
+  {
     this->processor_ = new LibRaw();
     Napi::Buffer<char> buff = info[0].As<Napi::Buffer<char>>();
     this->processor_->open_buffer(buff.Data(), buff.Length());
   }
-  else {
+  else
+  {
     Napi::TypeError::New(env, "LibRaw constructor did not receive required parameters.").ThrowAsJavaScriptException();
   }
 }
@@ -51,7 +63,6 @@ Napi::Value LibRawWrapper::GetThumbnail(const Napi::CallbackInfo& info) {
   Napi::Env env = info.Env();
   Napi::Object o = Napi::Object::New(env);
   
-
   this->processor_->unpack_thumb();
   if (this->processor_->imgdata.thumbnail.thumb)
   {
