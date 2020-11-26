4e32aebee2b28c14628ce6e6fdd0683250ad7bef
Justin Kambic <jek5104@gmail.com>
2020-11-25 23:01:51 -0500

    Install babel and associated packages for jest to handle TS.
    Update top-level syntax to jive with babel transpilation.
    
    Add sample test.
    
    Add first test.
    
    Tmp commit.
    
    Convert tests to typescript.

---
+++ test/libraw.test.ts
@@ -0,0 +1,39 @@
+import { LibRaw } from '../src/libraw';
+import path from 'path';
+import fs from 'fs';
+
+describe("LibRaw", () => {
+  test("basic metadata fields supported", async () => {
+    expect.assertions(3);
+    const lr = new LibRaw();
+    await lr.readFile(
+      path.join(__dirname, "test_images/RAW_SONY_ILCA-77M2.ARW")
+    );
+    const metadata = await lr.getMetadata();
+    expect(metadata.idata).toMatchSnapshot();
+    expect(metadata.lens).toMatchSnapshot();
+    expect(metadata.other).toMatchSnapshot();
+  });
+
+  test("reads img data from buffer", async () => {
+    expect.assertions(3);
+    const lr = new LibRaw();
+    const buffer = fs.readFileSync(
+      path.join(__dirname, "test_images/RAW_SONY_ILCA-77M2.ARW")
+    );
+    await lr.readBuffer(buffer);
+    const metadata = await lr.getMetadata();
+    expect(metadata.idata).toMatchSnapshot();
+    expect(metadata.lens).toMatchSnapshot();
+    expect(metadata.other).toMatchSnapshot();
+  });
+
+  test("parses xmp", async () => {
+    const lr = new LibRaw();
+    await lr.readFile(
+      path.join(__dirname, "test_images/RAW_NIKON_Z6.NEF")
+    );
+    const xmp = (await lr.getXmp()).toString('utf8');
+    expect(xmp).toMatchSnapshot();
+  });
+});
