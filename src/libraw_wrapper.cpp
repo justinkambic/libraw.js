0de0a1141e0ffdbf8dcb4797c3866d6175a90828
Justin Kambic <jek5104@gmail.com>
2020-12-15 22:42:47 -0500

    Delete commented code, simplify a function.

--- src/libraw_wrapper.cpp
+++ src/libraw_wrapper.cpp
@@ -36,43 +36,6 @@ Napi::Object LibRawWrapper::Init(Napi::Env& env, Napi::Object& exports) {
 LibRawWrapper::LibRawWrapper(const Napi::CallbackInfo& info): Napi::ObjectWrap<LibRawWrapper>(info) {
   this->processor_ = new LibRaw();
 }
-// LibRawWrapper::LibRawWrapper(const Napi::CallbackInfo& info): Napi::ObjectWrap<LibRawWrapper>(info) {
-//   Napi::Env env = info.Env();
-//   Napi::HandleScope scope(env);
-
-//   if (!info[0].IsString() && (!info[0].IsBuffer() && info.Length() > 1))
-//   {
-//     Napi::TypeError::New(env, "Expected path to image or buffer with size").ThrowAsJavaScriptException();
-//     return;
-//   }
-
-//   if (info.Length() == 0)
-//   {
-//     this->processor_ = new LibRaw();
-//   }
-//   else if (info[0].IsString())
-//   {
-//     Napi::String path = info[0].As<Napi::String>();
-//     std::fstream f(path.Utf8Value().c_str());
-//     if (!f.good())
-//     {
-//       Napi::Error::New(env, "File does not exist").ThrowAsJavaScriptException();
-//       return;
-//     }
-//     this->processor_ = new LibRaw();
-//     this->processor_->open_file(path.Utf8Value().c_str());
-//   }
-//   else if (info[0].IsBuffer() && info[1].IsNumber())
-//   {
-//     this->processor_ = new LibRaw();
-//     Napi::Buffer<char> buff = info[0].As<Napi::Buffer<char>>();
-//     this->processor_->open_buffer(buff.Data(), buff.Length());
-//   }
-//   else
-//   {
-//     Napi::TypeError::New(env, "LibRaw constructor did not receive required parameters.").ThrowAsJavaScriptException();
-//   }
-// }
 
 Napi::Value LibRawWrapper::GetThumbnail(const Napi::CallbackInfo& info) {
   Napi::Env env = info.Env();
@@ -94,19 +57,17 @@ Napi::Value LibRawWrapper::GetThumbnail(const Napi::CallbackInfo& info) {
 
 Napi::Value LibRawWrapper::GetXmpData(const Napi::CallbackInfo& info) {
   Napi::Env env = info.Env();
-  Napi::Object o = Napi::Object::New(env);
 
   char *xmp = this->processor_->imgdata.idata.xmpdata;
   if (xmp)
   {
-    Napi::Buffer<char> b = Napi::Buffer<char>::New(
+    return Napi::Buffer<char>::New(
       env,
       xmp,
       this->processor_->imgdata.idata.xmplen
     );
-    return b;
   }
-  return o;
+  return Napi::Object::New(env);
 }
 
 Napi::Value LibRawWrapper::GetMetadata(const Napi::CallbackInfo& info) {
